9e0893df3daa68c501f286394fb2fa94ede1f396
SiFou01 <seifbaghdad01@gmail.com>
2023-10-07 11:25:19 +0100

    Add play game function with prompt and score

--- script.js
+++ script.js
@@ -17,16 +17,31 @@ function getComputerChoice() {
 // Play one round
 function playRound(playerChoice, computerChoice) {
     computerChoice = getComputerChoice();
-    playerChoice = playerChoice.toLowerCase();
     if (playerChoice === computerChoice) {
         return "Draw";
     }
     else if (playerChoice === "rock" && computerChoice === "scissors"
              || playerChoice === "paper" && computerChoice === "rock"
              || playerChoice === "scissors" && computerChoice === "paper") {
+        playerScore++;
         return `You win ${playerChoice} beats ${computerChoice}`;
     }
     else {
+        computerScore++;
         return `You lose ${computerChoice} beats ${playerChoice}`;
     }
+}
+
+// Play Game 
+
+let playerScore = 0;
+let computerScore = 0;
+
+for (let i = 0; i < 5; i++) {
+let playerChoice = prompt(`Rock , Paper or Scissors`);
+playerChoice = playerChoice.toLowerCase();
+if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+    alert(playRound(playerChoice) + `
+    Score:
+    You: ${playerScore}      Computer: ${computerScore}`);}
 }
