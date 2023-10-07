c20583b94c4956905f86fb1503b6ba7980921251
SiFou01 <seifbaghdad01@gmail.com>
2023-10-07 10:56:23 +0100

    Add play round function

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ function getComputerChoice() {
     let num = Math.floor(Math.random() * 3 + 1)
     switch (num) {
         case 1 : 
-         computerChoice = "Rock";
+         computerChoice = "rock";
          break;
         case 2 :
-         computerChoice = "Paper";
+         computerChoice = "paper";
          break;
         case 3 :
-         computerChoice = "Scissors"
+         computerChoice = "scissors"
+    }
+    return computerChoice;
+}
+// Play one round
+function playRound(playerChoice, computerChoice) {
+    computerChoice = getComputerChoice();
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice === computerChoice) {
+        return "Draw";
+    }
+    else if (playerChoice === "rock" && computerChoice === "scissors"
+             || playerChoice === "paper" && computerChoice === "rock"
+             || playerChoice === "scissors" && computerChoice === "paper") {
+        return `You win ${playerChoice} beats ${computerChoice}`;
+    }
+    else {
+        return `You lose ${computerChoice} beats ${playerChoice}`;
     }
 }
